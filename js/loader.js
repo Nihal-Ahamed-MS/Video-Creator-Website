9ee216c131e16e5a37d16cd9eafcbd0e16eb1138
LUCIFER-dev-king <nihaljesiya@gmail.com>
2020-11-30 15:35:33 +0530

    Formspree backend is added to contact form

--- js/loader.js
+++ js/loader.js
@@ -19,20 +19,42 @@ function toogleModal(){
 }
 
 
+window.addEventListener("DOMContentLoaded", function() {
 
-document.getElementById('contact-form').addEventListener('submit',submitform);
+    
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
+    
+    function success() {
+      form.reset();
+      status.classList.add('success');
+      status.innerHTML = "Thanks!";
+    }
 
-function submitform(e){
-    e.preventDefault();
+    function error() {
+      status.classList.add('error');
+      status.innerHTML = "Oops! There was a problem.";
+    }
 
-    var name = getInputValue('name')
-    var email = getInputValue('email')
-    var phone = getInputValue('phone')
-    var message = getInputValue('message')
 
-    console.log(name)
-}
-
-function getInputValue(id){
-    return document.getElementById(id).value;
-}
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
